6ee4a5126347d9f55cb27ddebe7cf47e6917b946
VikaLiashenko <82883964+VikaViashenko@users.noreply.github.com>
2023-02-28 10:48:27 +0200

    Add project files.

---
+++ VolleyballTeam/Controllers/JoinController.cs
@@ -0,0 +1,23 @@
+﻿using VolleyballTeam.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VolleyballTeam.Controllers
+{
+    public class JoinController : Controller
+    {
+        public IActionResult Index() // join
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Check(Join join)
+        {
+            if (ModelState.IsValid)
+            {
+                return Redirect("/");
+            }
+            return View("Index");
+        }
+    }
+}
---
+++ VolleyballTeam/Models/Join.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+
+namespace VolleyballTeam.Models
+{
+    public class Join
+    {
+        [Display(Name = "Ім'я")]
+        [Required(ErrorMessage = "Введіть ім'я!")]
+        public string Name { get; set; }
+
+        [Display(Name = "Прізвище")]
+        [Required(ErrorMessage = "Введіть прізвище!")]
+        public string Surname { get; set; }
+
+        [Display(Name = "Стать")]
+        public string Sex { get; set; }
+
+        [Display(Name = "Вік")]
+        [Required(ErrorMessage = "Введіть ваш вік!")]
+        public int Age { get; set; }
+
+        [Display(Name = "Номер телефону")]
+        [Required(ErrorMessage = "Введіть номер телефону!")]
+        public int Phone { get; set; }
+
+        [Display(Name = "Імейл")]
+        public string Email { get; set; }
+
+        [Display(Name = "Ваш ріст")]
+        public int Height { get; set; }
+
+        [Display(Name = "Вашу вагу")]
+        public int Weight { get; set; }
+
+        [Display(Name = "Коментар")]
+        [StringLength(100, ErrorMessage = "Ваш коментар не може містити більше ніж 100 символів")]
+        public string Comment { get; set; }
+
+    }
+}
